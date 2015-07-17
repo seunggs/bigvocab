30ef7ee5f862219af22e7d021fdf08e8eb3f5d64
seunggs <seunggs@gmail.com>
2015-07-16 22:54:41 -0400

    remove rethinkdbdash

--- server/config/rdbdash.js
+++ server/config/rdbdash.js
@@ -1,16 +1,8 @@
 'use strict';
 
-var rethinkdbdash = require('rethinkdbdash');
+var r = require('rethinkdb');
 
 var config = require('./default');
-
-var r = rethinkdbdash({
-	db: config.rethinkdb.db, 
-	servers: [{
-		host: config.rethinkdb.host,
-		port: config.rethinkdb.port
-	}]
-});
 
 r.connect({ host: config.rethinkdb.host, port: config.rethinkdb.port }, function(err, conn) {
   console.log(err);
