930280a4d3398a26bc7c95c16f8e2285fa496e5f
seunggs <seunggs@gmail.com>
2015-07-16 23:01:45 -0400

    attempt to fix rethinkdb

--- server/config/rdbdash.js
+++ server/config/rdbdash.js
@@ -4,9 +4,4 @@ var r = require('rethinkdb');
 
 var config = require('./default');
 
-r.connect({ host: config.rethinkdb.host, port: config.rethinkdb.port }, function(err, conn) {
-  console.log(err);
-  console.log(conn);
-});
-
-module.exports = r;
+module.exports = r.connect({ host: config.rethinkdb.host, port: config.rethinkdb.port });
