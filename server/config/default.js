df8e7e1a03f675d4653c5749e3b7d48cf6330ca3
seunggs <seunggs@gmail.com>
2015-07-20 22:51:23 -0400

    test

--- server/config/default.js
+++ server/config/default.js
@@ -3,7 +3,7 @@
 module.exports = {
 	port: process.env.PORT || 8000,
 	rethinkdb: {
-		host: process.env.RDB_HOST,
+		host: process.env.RDB_HOST || localhost,
 		port: process.env.RDB_PORT,
 		db: 'bigvocab'
 	}
