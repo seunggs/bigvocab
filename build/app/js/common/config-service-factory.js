0f945434238a8d7f1c612181a1ee188a3576587f
seunggs <seunggs@gmail.com>
2015-08-04 18:12:43 -0400

    add explanations for review responses

---
+++ build/app/js/common/config-service-factory.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc service
+   * @name common.factory:ConfigService
+   *
+   * @description
+   *
+   */
+  angular.module('common').factory('ConfigService', ConfigService);
+
+  function ConfigService() {
+
+    var ConfigServiceBase = {};
+
+    ConfigServiceBase.mashapeKey = 'S4DvXSr43Cmsh5Kww0kOuX9QxNbXp1hjhkYjsn84TFraf8SlG3';
+    ConfigServiceBase.forvoKey = 'f850390038cd293954cb5bdf7dc36d8a';
+
+    return ConfigServiceBase;
+  }
+})();
+//# sourceMappingURL=../common/config-service-factory.js.map
---
+++ build/app/js/main-app/collections-service-factory.js
@@ -0,0 +1,46 @@
+'use strict';
+
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc service
+   * @name mainApp.factory:CollectionsService
+   *
+   * @description
+   *
+   */
+  angular.module('mainApp').factory('CollectionsService', CollectionsService);
+
+  function CollectionsService($http) {
+
+    var CollectionsServiceBase = {};
+
+    CollectionsServiceBase.getAll = function (userId) {
+      return $http.get('/api/collections/' + userId);
+    };
+
+    CollectionsServiceBase.create = function (collection) {
+      return $http.post('/api/collections', collection);
+    };
+
+    CollectionsServiceBase.get = function (collectionId) {
+      return $http.get('/api/collections/' + collectionId);
+    };
+
+    CollectionsServiceBase.merge = function (collectionId, newCollectionId) {
+      return $http.put('/api/collections/merge/' + collectionId, newCollectionId);
+    };
+
+    CollectionsServiceBase.update = function (collectionId, newCollection) {
+      return $http.put('/api/collections/' + collectionId, newCollection);
+    };
+
+    CollectionsServiceBase['delete'] = function (collectionId) {
+      return $http['delete']('/api/collections/' + collectionId);
+    };
+
+    return CollectionsServiceBase;
+  }
+})();
+//# sourceMappingURL=../main-app/collections-service-factory.js.map
