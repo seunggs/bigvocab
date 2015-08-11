d31f67319361041182c1c80e1d7dd2e898823ff4
seunggs <seunggs@gmail.com>
2015-08-11 09:53:46 -0400

    bug fix

---
+++ build/app/js/main-app/main-app-module.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name mainApp
+   * @description
+   *
+   */
+  angular.module('mainApp', ['ui.router']);
+})();
+//# sourceMappingURL=../main-app/main-app-module.js.map
